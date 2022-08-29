4ca66e76ea9b53a05140a17e596e063d05e31d31
Izmaylova <Natalyatf@yandex.ru>
2022-08-30 00:11:44 +0500

    Решение задач 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Задача 1
         System.out.println("Задача 1");
+
         byte dog = 5;
         System.out.println("Значение переменной " + "Собака " + "с типом"+ " byte равно " + dog);
         int cat = 30;
@@ -19,6 +20,7 @@ public class Main {
 
         // Задача 2
         System.out.println("Задача 2");
+
         double a = 27.12;
         System.out.println(a);
         long b = 987678965549L;
@@ -38,6 +40,7 @@ public class Main {
 
         // Задача 3
         System.out.println("Задача 3");
+
         byte LudmilaPavlovnaNumberOfStudents = 23;
         byte AnnaSergeevnaNumberOfStudents = 27;
         byte EkaterinaAndreevnaNumberOfStudents = 30;
@@ -51,6 +54,7 @@ public class Main {
 
         // Задача 4
         System.out.println("Задача 4");
+
         byte machinePerfomanceModeBottles = 16;
         byte machinePerfomanceModeTime = 2;
         System.out.println("Машина изготавливает в минуту " +
@@ -77,6 +81,74 @@ public class Main {
 
         // Задача 5
         System.out.println("Задача 5");
+
+        byte paintCansTotal = 120;
+        byte cansOfWhitePaintForThe1StClass = 2;
+        byte cansOfBrownPaintForThe1StClass = 4;
+        System.out.println("В школе, где " + (paintCansTotal/
+                (cansOfBrownPaintForThe1StClass+cansOfWhitePaintForThe1StClass)) + " классов, нужно "
+                + (cansOfWhitePaintForThe1StClass*
+                (paintCansTotal/(cansOfBrownPaintForThe1StClass+cansOfWhitePaintForThe1StClass)))
+                + " банок белой краски и "
+                + (cansOfBrownPaintForThe1StClass*
+                (paintCansTotal/(cansOfBrownPaintForThe1StClass+cansOfWhitePaintForThe1StClass)))
+                + " банок коричневой краски");
+
+        // Задача 6
+        System.out.println("Задача 6");
+
+        int bananaWeightOnGram = 80;
+        int milkWeightOn100Ml = 105;
+        int iceCreamWeight = 100;
+        int eggWeight = 70;
+
+        int totalWeightOnGram = 5*bananaWeightOnGram
+                + 2* milkWeightOn100Ml
+                + 2*iceCreamWeight
+                + 4*eggWeight;
+
+        System.out.println("Вес всех продуктов равен " + (totalWeightOnGram / 1000.0) + " кг");
+
+        // Задача 7
+        System.out.println("Задача 7");
+
+        int requiredWeightWeightOnGram = 7000;
+        double minimumWeightLostInDay = 250;
+        double maximumWeightLostInDay = 500;
+
+        System.out.println("При минимальной нагрузке спортсмен похудеет за "
+                + (requiredWeightWeightOnGram/minimumWeightLostInDay) + " дней");
+        System.out.println("При максимальной нагрузке спортсмен похудеет за "
+                + (requiredWeightWeightOnGram/maximumWeightLostInDay) + " дней ");
+
+        // Задача 8
+        System.out.println("Задача 8");
+
+        int mashasalary = 67_760;
+        int denisSalary = 83_690;
+        int kristinaSalary = 76_230;
+        var percent = 1.1;
+        var numberOfMonth = 12;
+        double mashaNewSalary = mashasalary*percent;
+        double denisNewSalary = denisSalary*percent;
+        double kristinaNewSalary = kristinaSalary*percent;
+
+        System.out.println("Маша теперь получает " + mashasalary* percent + " рублей в месяц");
+        System.out.println("Денис теперь получает " + denisSalary* percent + " рублей в месяц");
+        System.out.println("Кристина теперь получает " + kristinaSalary* percent + " рублей в месяц");
+        int mashaOldAnnualSalary = mashasalary*numberOfMonth;
+        int denisOldAnnualSalary = denisSalary*numberOfMonth;
+        int kristinaOldAnnualSalary = kristinaSalary*numberOfMonth;
+
+        System.out.println("Годовой доход Маши вырос на " +
+                ((mashaNewSalary*numberOfMonth)-mashaOldAnnualSalary) + " рублей");
+        System.out.println("Годовой доход Дениса вырос на " +
+                ((denisNewSalary*numberOfMonth)-denisOldAnnualSalary) + " рублей");
+        System.out.println("Годовой доход Кристины вырос на " +
+                ((kristinaNewSalary*numberOfMonth)-kristinaOldAnnualSalary) + " рублей");
+
+
+
 
 
 
